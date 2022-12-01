159bac2db94cdb98ce4f98e07c4c41e64815a612
tomasmik <tomasmik@protonmail.com>
2022-12-01 13:38:01 +0200

    More e2e tests

--- e2e/yaml_test.go
+++ e2e/yaml_test.go
@@ -6,34 +6,33 @@ import (
 	"time"
 
 	. "github.com/onsi/gomega"
-
 	"github.com/spacelift-io/celplate"
 	"github.com/spacelift-io/celplate/evaluator"
 )
 
 func TestEndToEnd(t *testing.T) {
-	RegisterTestingT(t)
-
+	wt := NewWithT(t)
 	evaluator, err := evaluator.NewCEL(map[string]map[string]any{
 		"inputs": {
 			"environment": "production",
 			"region":      "us-east-1",
 			"id":          1,
+			"serial":      111111111111,
 		},
 		"context": {
 			"datetime": time.Date(2022, time.April, 10, 1, 1, 1, 1, time.UTC),
 		},
 	})
-	Expect(err).ToNot(HaveOccurred())
+	wt.Expect(err).ToNot(HaveOccurred())
 
 	input, err := os.ReadFile("fixtures/input.yaml")
-	Expect(err).ToNot(HaveOccurred())
+	wt.Expect(err).ToNot(HaveOccurred())
 
 	output, err := os.ReadFile("fixtures/output.yaml")
-	Expect(err).ToNot(HaveOccurred())
+	wt.Expect(err).ToNot(HaveOccurred())
 
 	out, err := celplate.NewScanner(evaluator).Transform(input)
 
-	Expect(err).ToNot(HaveOccurred())
-	Expect(string(out)).To(Equal(string(output)))
+	wt.Expect(err).ToNot(HaveOccurred())
+	wt.Expect(string(out)).To(Equal(string(output)))
 }
