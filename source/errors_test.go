a9d685638e82062eb5f9ec2c107783b69dd5a9b7
kskitek <krzysztofs@spacelift.io>
2026-06-29 15:42:00 +0200

    chore: Replace goblin and gomega with testify (#18)

--- source/errors_test.go
+++ source/errors_test.go
@@ -4,33 +4,28 @@ import (
 	"errors"
 	"testing"
 
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/spacelift-io/celplate/source"
 )
 
 func TestErrors(t *testing.T) {
-	RegisterTestingT(t)
-
 	sut := &source.Errors{}
 
-	// No errors is nil
-	Expect(sut.ErrorOrNil()).To(BeNil())
+	require.NoError(t, sut.ErrorOrNil())
 
-	// Push 3 errors, two of type source
 	serr1 := &source.Error{Location: source.Location{Line: 1, Column: 2}, Message: "foo"}
 	serr2 := &source.Error{Location: source.Location{Line: 3, Column: 4}, Message: "bar"}
 	sut.Push(serr1)
 	sut.Push(serr2)
 	sut.Push(errors.New("internal error"))
 
-	// Check for the erors
-	Expect(sut.ErrorOrNil()).ToNot(BeNil())
-	Expect(sut.Error()).To(Equal("line 1, column 2: foo; line 3, column 4: bar; internal error"))
+	require.Error(t, sut.ErrorOrNil())
+	assert.Equal(t, "line 1, column 2: foo; line 3, column 4: bar; internal error", sut.Error())
 
-	// Get only the source errors
 	result := source.GetErrors(sut)
-	Expect(len(result)).To(Equal(2))
-	Expect(result[0]).To(Equal(serr1))
-	Expect(result[1]).To(Equal(serr2))
+	require.Len(t, result, 2)
+	assert.Equal(t, serr1, result[0])
+	assert.Equal(t, serr2, result[1])
 }
--- source/location_test.go
+++ source/location_test.go
@@ -3,64 +3,33 @@ package source_test
 import (
 	"testing"
 
-	"github.com/franela/goblin"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/spacelift-io/celplate/source"
 )
 
-func TestLocation(t *testing.T) {
-	g := goblin.Goblin(t)
-	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
+func TestLocation_Start(t *testing.T) {
+	sut := source.Start()
+	assert.Zero(t, sut.Index)
+	assert.Equal(t, "line 1, column 1", sut.String())
+}
 
-	var sut *source.Location
+func TestLocation_Advance_RegularCharacter(t *testing.T) {
+	sut := source.Start()
+	sut.Advance('a')
+	assert.Equal(t, 1, sut.Index)
+	assert.Equal(t, "line 1, column 2", sut.String())
+}
 
-	g.Describe("Location", func() {
-		g.BeforeEach(func() { sut = source.Start() })
+func TestLocation_Advance_LineBreak(t *testing.T) {
+	sut := source.Start()
+	sut.Advance('\n')
+	assert.Equal(t, 1, sut.Index)
+	assert.Equal(t, "line 2, column 1", sut.String())
+}
 
-		g.Describe("Start", func() {
-			g.It("should return a starting location", func() {
-				Expect(sut.Index).To(BeZero())
-				Expect(sut.String()).To(Equal("line 1, column 1"))
-			})
-		})
-
-		g.Describe("Advance", func() {
-			var char rune
-
-			g.JustBeforeEach(func() { sut.Advance(char) })
-
-			g.Describe("with a regular character", func() {
-				g.BeforeEach(func() { char = 'a' })
-
-				g.It("should advance the column", func() {
-					Expect(sut.Index).To(Equal(1))
-					Expect(sut.String()).To(Equal("line 1, column 2"))
-				})
-			})
-
-			g.Describe("with a line break", func() {
-				g.BeforeEach(func() { char = '\n' })
-
-				g.It("should advance the line", func() {
-					Expect(sut.Index).To(Equal(1))
-					Expect(sut.String()).To(Equal("line 2, column 1"))
-				})
-			})
-		})
-
-		g.Describe("Nested", func() {
-			var nested source.Location
-
-			g.BeforeEach(func() {
-				sut = &source.Location{Index: 1, Line: 2, Column: 3}
-			})
-
-			g.JustBeforeEach(func() { nested = source.Location{Index: 1, Line: 2, Column: 3} })
-
-			g.It("should return a new location that is nested within the current location", func() {
-				Expect(sut.Nested(nested)).To(Equal(source.Location{Index: 2, Line: 3, Column: 5}))
-			})
-		})
-	})
+func TestLocation_Nested(t *testing.T) {
+	sut := &source.Location{Index: 1, Line: 2, Column: 3}
+	nested := source.Location{Index: 1, Line: 2, Column: 3}
+	assert.Equal(t, source.Location{Index: 2, Line: 3, Column: 5}, sut.Nested(nested))
 }
